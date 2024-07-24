3425fd0764f5eeafa2ffbae9084849db3549f2bb
League and Chill <enzo.marchetti97@gmail.com>
2024-07-24 19:08:40 +0200

    Implemented crouching

--- RangerProject/Assets/RangerProject/Scripts/Player/PlayerController.cs
+++ RangerProject/Assets/RangerProject/Scripts/Player/PlayerController.cs
@@ -8,19 +8,26 @@ namespace RangerProject.Scripts.Player
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private float MovementSpeed = 10.0f;
+        [SerializeField] private float MovementSpeedCrouching = 10.0f;
         [SerializeField] private float Gravity = -9.81f;
         [SerializeField] private float GravityMultiplier = 1.0f;
-        [SerializeField] private float JumpForce = 10.0f; 
+        [SerializeField] private float JumpForce = 10.0f;
+        [SerializeField, Range(0, 1.0f)] private float PlayerCrouchHeightMultiplier = 0.7f;
+        [SerializeField] private string GroundTag = "Ground";
+        [SerializeField] private CapsuleCollider PlayerCapsule;
         [SerializeField] private PlayerInput PlayerInput;
         [SerializeField] private Rigidbody PlayerRB;
-        [SerializeField] private string GroundTag = "Ground";
 
+        private float DefaultCapsuleHeight = 0.0f;
+        
         private bool IsGrounded = false;
+        private bool IsCrouching = false;
         private Vector3 Velocity;
         
         private void Start()
         {
             PlayerInput.camera = Camera.current;
+            DefaultCapsuleHeight = PlayerCapsule.height;
         }
 
         private void FixedUpdate()
@@ -40,10 +47,34 @@ namespace RangerProject.Scripts.Player
         public void OnWalk(InputAction.CallbackContext CallbackContext)
         {
             Vector2 WalkInput = CallbackContext.ReadValue<Vector2>();
-            Velocity = new Vector3(WalkInput.x * MovementSpeed, Velocity.y, WalkInput.y * MovementSpeed);
+
+            float XMovement = WalkInput.x;
+            float ZMovement = WalkInput.y;
+            
+            Velocity = new Vector3(XMovement, Velocity.y, ZMovement);
         }
 
-        void ApplyGravity()
+        public void OnCrouch(InputAction.CallbackContext CallbackContext)
+        {
+            if (CallbackContext.started)
+            {
+                Debug.Log("Started Crouching");
+                IsCrouching = true;
+                
+                float NewPlayerCapsuleHeight = PlayerCapsule.height * PlayerCrouchHeightMultiplier;
+               
+                PlayerCapsule.height = NewPlayerCapsuleHeight;
+                PlayerCapsule.center += Vector3.down * (1 - PlayerCrouchHeightMultiplier);
+            }
+            else if (CallbackContext.canceled)
+            {
+                Debug.Log("Ended crouching");
+                IsCrouching = false;
+                PlayerCapsule.height = DefaultCapsuleHeight;
+                PlayerCapsule.center = Vector3.zero;
+            }
+        }
+        private void ApplyGravity()
         {
             if (IsGrounded && Velocity.y < 0)
             {
@@ -54,10 +85,13 @@ namespace RangerProject.Scripts.Player
                 Velocity.y += Gravity * GravityMultiplier * Time.deltaTime;
             }
         }
-
-        void ApplyMovement()
+        
+        private void ApplyMovement()
         {
-            PlayerRB.velocity = Velocity;
+            float XMovement = Velocity.x * (IsCrouching ? MovementSpeedCrouching : MovementSpeed);
+            float ZMovement = Velocity.z * (IsCrouching ? MovementSpeedCrouching : MovementSpeed);
+            
+            PlayerRB.velocity = new Vector3(XMovement, Velocity.y, ZMovement);
         }
 
         private void OnCollisionEnter(Collision other)
